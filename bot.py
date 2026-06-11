884fdeda8d9ce6f6960487ef3674f5b275d845c8
janimentu <janimentu@hotmail.com>
2026-06-11 06:28:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def send(msg):
     })
 
 def get_yesterday():
-    return "2025-12-15"  # vaihda takaisin: return (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%d")
+    return (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 def get_games(date):
     return requests.get(f"https://api-web.nhle.com/v1/score/{date}").json()
